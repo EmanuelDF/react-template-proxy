f660c1f53b251419c596f6f092bce3eceff48542
Emanuel Galhardo <emanuel.df@gmail.com>
2018-09-21 10:50:24 -0300

    Update App.js

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Que lindo, Jow! Você rodou finalmente usando proxy.</Text>
+        <Text style={styles.welcome}>Que lindo! Você rodou finalmente usando proxy.</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
